a80244e04cdbf416ba1faf3dbdac254a4506fe26
aldesse <Bussiere.alexandre@gmail.com>
2024-11-04 11:16:31 +0100

    Sa marche ?

--- TCG/card-collection-service/src/main/java/com/TCG/card_collection_service/service/GrpcClientService.java
+++ TCG/card-collection-service/src/main/java/com/TCG/card_collection_service/service/GrpcClientService.java
@@ -6,6 +6,8 @@ import com.example.lib.CardServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class GrpcClientService {
@@ -13,24 +15,29 @@ public class GrpcClientService {
     private final CardServiceGrpc.CardServiceBlockingStub grpcServiceStub;
 
     public GrpcClientService() {
+
+        Logger logger = LoggerFactory.getLogger(GrpcClientService.class);
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 9090) // Adresse et port de votre service gRPC
                 .usePlaintext()
                 .build();
-
+        logger.info("gRPC channel created successfully");
         this.grpcServiceStub = CardServiceGrpc.newBlockingStub(channel);
+
     }
 
     public String callCardExchange(String idDeJoueur, String idDeCarte) {
+        Logger logger = LoggerFactory.getLogger(GrpcClientService.class);
         // Construire la requête gRPC
-        CardExchangeRequest request = CardExchangeRequest.newBuilder()
-                .setIdDeJoueur(idDeJoueur)
-                .setIdDeCarte(idDeCarte)
-                .build();
-
-        // Appeler le service gRPC et récupérer la réponse
-        CardExchangeResponse response = grpcServiceStub.cardExchange(request);
-
-        return response.getMessage(); // Retourne le message de confirmation ou d'erreur
+        logger.info("Preparing to call gRPC method 'cardExchange' with idDeJoueur={} and idDeCarte={}", idDeJoueur,
+                idDeCarte);
+        // CardExchangeRequest request = CardExchangeRequest.newBuilder()
+        // .setIdDeJoueur(idDeJoueur)
+        // .setIdDeCarte(idDeCarte)
+        // .build();
+        // CardExchangeResponse response = grpcServiceStub.cardExchange(request);
+        String responses = "Success: true, Message: Card exchange successful"; // POUR TESTER NE PAS OUBLIER DE RETABLIR
+                                                                               // SE FICHIER POUR L'APPELLE
+        return responses;
     }
 }
